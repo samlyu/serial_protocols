17a75c4d54a7e68c7986e8c2904f6875d30a2b7c
samlyu <lvshengwei.samuel@gmail.com>
2019-10-11 17:57:41 +0800

    uart rx last commit

--- UART/DUT/uart_rx.v
+++ UART/DUT/uart_rx.v
@@ -131,7 +131,7 @@ always@(*) begin
 				next_state = (bit_count == DATA_WIDTH && br_clk) ? STOP : DATA;
 		end
 		PARI:	next_state = br_clk ? STOP : PARI;
-		STOP:	next_state = (br_clk & rx_sample[1]) ? DONE : STOP;
+		STOP:	next_state = br_clk ? DONE : STOP;
 		DONE:	next_state = IDLE;
 		default:	next_state = IDLE;
 	endcase
@@ -193,7 +193,7 @@ always@(posedge clk or negedge arstn) begin
 				rx_done <= 1'b0;
 				clk_count_en <= 1'b1;
 				if(sample_count == 'd6) begin
-					if(~RXD)	rx_error <= 1'b1;
+					if(~rx_sample[1])	rx_error <= 1'b1;
 				end
 			end
 			DONE:	begin
